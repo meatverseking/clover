138434e03b298988d67656f4e74fb6abc8530e95
george-hub331 <idiaghegeorge9@gmail.com>
2022-06-25 00:06:54 +0100

    [update]: add init

---
+++ pages/dashboard/index.tsx
@@ -0,0 +1,83 @@
+
+import { Web3Storage } from "web3.storage";
+
+import {
+  LinearProgress
+} from "@mui/material";
+
+
+ const makeStorageClient = async () => {
+        // return new Web3Storage({
+        //   token: ''
+        // });
+      }
+
+      const beginUpload = async (files:FileList) => {
+        const { size: totalSize } = files[0];
+    
+
+        const onRootCidReady = (cid:string) => {
+        
+            
+    
+        };
+
+        let uploaded = 0;
+
+        const onStoredChunk = (size: number) => {
+          uploaded += size;
+
+          const pct = (totalSize / uploaded) * 100;
+
+          console.log(`Uploading... ${pct.toFixed(2)}% complete`);
+
+        };
+
+        const client = await makeStorageClient();
+
+        return client.put(files, { onRootCidReady, onStoredChunk });
+      }; 
+
+const Dashboard = () => {
+
+    const sendStuff = (files:FileList) => {
+        
+        const blobFiles = [];
+        
+        for(let i:number = 0;i<files.length; i++) {
+
+        let startPointer = 0;
+        let endPointer = files[i].size;
+        let chunks = [];
+        while(startPointer<endPointer){
+            let newStartPointer = startPointer+(files[i].size/2);
+            chunks.push(files[i].slice(startPointer,newStartPointer));
+            startPointer = newStartPointer;
+        }
+
+         blobFiles.push(new Blob(chunks, { type: files[i].type }));              
+    }
+
+
+
+}
+
+    return (
+    
+    <div onDragOver={(event) => {
+          event.stopPropagation();
+          event.preventDefault();
+          event.dataTransfer.dropEffect = 'copy';
+    }} onDrop={(event) => {
+         event.stopPropagation();
+        event.preventDefault();
+     const fileList = event.dataTransfer.files;
+         sendStuff(fileList)
+    }} className="w-full filedrop h-screen">
+
+
+
+    </div>)
+}
+
+export default Dashboard;
---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./components/*.{js,jsx,ts,tsx}",
+    "./components/**/*.{js,jsx,ts,tsx}",
+    "./components/**/**/*.{js,jsx,ts,tsx}",
+    "./pages/**/*.{js,jsx,ts,tsx}",
+    "./pages/**/**/*.{js,jsx,ts,tsx}",
+    "./pages/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    screens: {
+      
+    },
+    extend: {
+    
+    },
+  },
+  plugins: [],
+};
