2ac05a59f76cd54ec2aba6a3869e52f93b748519
Temiloluwa Adelowo <moboluwaji003@gmail.com>
2022-06-25 12:20:06 +0100

    feat: scaffolding with Moralis

---
+++ app/components/dash.tsx
@@ -0,0 +1,15 @@
+import Head from 'next/head';
+function Dashboard() {
+  return (
+    <>
+      <Head>
+        <title>Your Dashboard | ClearCloud</title>
+        <meta name="description" content={`Cloud storage without limits`} />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div></div>
+    </>
+  )
+}
+
+export default Dashboard;
