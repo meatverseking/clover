238d9627bdec50ab7830574fe7859fddbc047714
george-hub331 <idiaghegeorge9@gmail.com>
2022-07-05 00:38:14 +0100

    [update]: complete body

--- app/components/folder/index.tsx
+++ app/components/folder/index.tsx
@@ -1,15 +1,61 @@
-import { FaFolder } from 'react-icons/fa'
+const Folder = ({ color = "#40A9FF", data }: { color?: string, data: {
+  name: string,
+  files: number,
+  size: number
+} }) => {
 
-function Folder() {
+  const { name, files, size} = data;
+
+  const getSize = () => {
+
+    const format = (_size: number, subsize:number, type: string): string => {
+        return (_size / subsize).toFixed(2) + type;
+    }
+
+    if(size > 1048576)
+        return format(size, 1048576, 'TB');
+    else if (size > 1024) 
+          return format(size, 1024, "GB");
+    else if(size > 1)
+          return format(size, 1, 'MB');
+    else if(size > 0.000977)
+          return format(size, 0.000977, 'KB');
+    else if (size > 0.000000977) 
+          return format(size, 0.000000977, 'B');
+  }
+
+  const mainSize = getSize();
+
   return (
-    <div className="flex flex-col">
+    <div className="flex m-auto flex-col w-[186px] h-[199px] py-4 px-2 justify-between items-center cursor-pointer border-[1px] border-solid border-transparent hover:bg-[#8b8b8b24] hover:border-[#e1e1e1] transition-all delay-300">
       <div className="icon">
-        <FaFolder color="#1890FF" className="w-[70px] h-[70px]" />
+        <svg
+          width="100"
+          height="75"
+          viewBox="0 0 100 75"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            fillRule="evenodd"
+            clipRule="evenodd"
+            d="M4 0C1.79086 0 0 1.79086 0 4V14C0 14.1693 0.0105217 14.3362 0.0309469 14.5C0.0105217 14.6638 0 14.8307 0 15V71C0 73.2091 1.79086 75 4 75H96C98.2091 75 100 73.2091 100 71V15C100 12.7909 98.2091 11 96 11H43.0278C43.0256 10.9981 43.0234 10.9962 43.0212 10.9942L31.6323 0.994239C30.9025 0.353396 29.9644 0 28.9931 0H4Z"
+            fill={color}
+          />
+        </svg>
       </div>
-      <div className="text-lg font-bold text-gray-800">Folder</div>
-      <div className="text-base font-medium text-gray-700">223MB</div>
+
+      <div className="text-[20px] max-h-[28px] truncate text-center w-full leading-7 font-[500] text-[#000000D9]">
+        {name}
+      </div>
+      <div className="text-[16px] text-center w-full truncate leading-6 font-bold text-[#00000073]">
+        {files} item{files > 1 ? "s" : ""}
+      </div>
+      <div className="text-[13px] text-center w-full -mt-2 truncate leading-6 font-bold text-[#00000040]">
+        {mainSize}
+      </div>
     </div>
-  )
+  );
 }
 
 export default Folder;
--- pages/dashboard/index.tsx
+++ pages/dashboard/index.tsx
@@ -2,45 +2,42 @@ import { Web3Storage } from "web3.storage";
 import Link from 'next/link';
 import {
   LinearProgress,
-  Button
+  Button,Paper, InputBase, IconButton
 } from "@mui/material"
 
+import Head from 'next/head';
+
 import { FaRegFolderOpen, FaPlus, FaFolder, FaTrash, FaRegClock } from 'react-icons/fa'
-import { BsCloudy } from 'react-icons/bs'
-import { TbUsers } from 'react-icons/tb'
-import { FaSearch } from "react-icons/fa";
-import * as React from "react";
-import Paper from "@mui/material/Paper";
-import InputBase from "@mui/material/InputBase";
-import IconButton from "@mui/material/IconButton";
+import { BsCloudy, BsGrid3X3Gap } from 'react-icons/bs'
+import { TbSearch, TbUsers } from 'react-icons/tb'
 import Folder from "../../app/components/folder";
 
 
-const makeStorageClient = async () => {
-  // return new Web3Storage({
-  //   token: ''
-  // });
-};
+// const makeStorageClient = async () => {
+//   // return new Web3Storage({
+//   //   token: ''
+//   // });
+// };
 
-const beginUpload = async (files: FileList) => {
-  const { size: totalSize } = files[0];
+// const beginUpload = async (files: FileList) => {
+//   const { size: totalSize } = files[0];
 
-  const onRootCidReady = (cid: string) => { };
+//   const onRootCidReady = (cid: string) => { };
 
-  let uploaded = 0;
+//   let uploaded = 0;
 
-  const onStoredChunk = (size: number) => {
-    uploaded += size;
+//   const onStoredChunk = (size: number) => {
+//     uploaded += size;
 
-    const pct = (totalSize / uploaded) * 100;
+//     const pct = (totalSize / uploaded) * 100;
 
-    console.log(`Uploading... ${pct.toFixed(2)}% complete`);
-  };
+//     console.log(`Uploading... ${pct.toFixed(2)}% complete`);
+//   };
 
-  const client = await makeStorageClient();
+//   const client = await makeStorageClient();
 
-  // return client.put(files, { onRootCidReady, onStoredChunk });
-};
+//   // return client.put(files, { onRootCidReady, onStoredChunk });
+// };
 
 const Dashboard = () => {
   //     const sendStuff = (files:FileList) => {
@@ -64,168 +61,209 @@ const Dashboard = () => {
   // }
 
   return (
-    <div
-      onDragOver={(event) => {
-        event.stopPropagation();
-        event.preventDefault();
-        event.dataTransfer.dropEffect = "copy";
-      }}
-      onDrop={(event) => {
-        event.stopPropagation();
-        event.preventDefault();
-        const fileList = event.dataTransfer.files;
-        //  sendStuff(fileList)
-    }} className="w-full flex items-start justify-between filedrop min-h-screen">
-    <div className="h-full bg-[#f5F5F5] sidebar fixed min-w-[236px] w-[236px]">
-      <div className="mt-3 mb-5">
-        <Link href="/">
-          <div className="text-[#1890FF] flex pl-4 items-center font-bold text-[18px]">
-            <FaFolder size={25} className="mr-5 flex" color={'#1890FF'}/> 
-             Virtual Drive
+    <>
+      <Head>
+        <title>user | Dashboard</title>
+      </Head>
+      <div
+        onDragOver={(event) => {
+          event.stopPropagation();
+          event.preventDefault();
+          event.dataTransfer.dropEffect = "copy";
+        }}
+        onDrop={(event) => {
+          event.stopPropagation();
+          event.preventDefault();
+          const fileList = event.dataTransfer.files;
+          //  sendStuff(fileList)
+        }}
+        className="w-full flex items-start justify-between filedrop min-h-screen"
+      >
+        <div className="h-full bg-[#f5F5F5] sidebar fixed min-w-[236px] w-[236px]">
+          <div className="mt-3 mb-5">
+            <Link href="/">
+              <div className="text-[#1890FF] flex pl-4 items-center font-bold text-[18px]">
+                <FaFolder size={25} className="mr-5 flex" color={"#1890FF"} />
+                Clear Cloud
+              </div>
+            </Link>
+          </div>
+
+          <div className="flex flex-col h-full">
+            <div className="cusscroller h-[calc(100% - 1.25rem - 0.75rem - 27px)] overflow-y-scroll overflow-x-hidden">
+              <div className="mb-2">
+                <span className="pl-4 font-[500] mb-3 block text-[#595959]">
+                  Drive Storage
+                </span>
+
+                <div className="flex pl-4 text-[14px] border-r-transparent items-center text-[rgba(0,0,0,0.45)]  hover:text-[#262626] py-2 cursor-pointer transition-all delay-500 hover:bg-[#bfbfbfe1] hover:border-r-[#5F5F5F] border-r-solid border-r-2 ">
+                  <FaRegFolderOpen className="mr-2" size={20} /> My Drive
+                </div>
+
+                <div className="flex pl-4 text-[14px] border-r-transparent items-center text-[rgba(0,0,0,0.45)]  hover:text-[#262626] py-2 cursor-pointer transition-all delay-500 hover:bg-[#bfbfbfe1] hover:border-r-[#5F5F5F] border-r-solid border-r-2 ">
+                  <TbUsers className="mr-2" size={20} /> Shared with me
+                </div>
+              </div>
+
+              <div className="mb-2">
+                <span className="pl-4 font-[500] text-[14px] mb-3 block text-[#595959]">
+                  Tags
+                </span>
+
+                <div className="flex pl-4 text-[14px] border-r-transparent items-center text-[rgba(0,0,0,0.45)]  hover:text-[#262626] py-2 cursor-pointer transition-all delay-500 hover:bg-[#bfbfbfe1] hover:border-r-[#5F5F5F] border-r-solid border-r-2 ">
+                  <div className="h-[14px] w-[14px] mr-2 rounded-[50%] bg-[#FF4D4F]"></div>{" "}
+                  Red
+                </div>
+
+                <div className="flex pl-4 text-[14px] border-r-transparent items-center text-[rgba(0,0,0,0.45)]  hover:text-[#262626] py-2 cursor-pointer transition-all delay-500 hover:bg-[#bfbfbfe1] hover:border-r-[#5F5F5F] border-r-solid border-r-2 ">
+                  <div className="h-[14px] w-[14px] mr-2 rounded-[50%] bg-[#FADB14]"></div>{" "}
+                  Yellow
+                </div>
+
+                <div className="flex pl-4 text-[14px] border-r-transparent items-center text-[rgba(0,0,0,0.45)]  hover:text-[#262626] py-2 cursor-pointer transition-all delay-500 hover:bg-[#bfbfbfe1] hover:border-r-[#5F5F5F] border-r-solid border-r-2 ">
+                  <div className="h-[14px] w-[14px] mr-2 rounded-[50%] bg-[#40A9FF]"></div>{" "}
+                  Blue
+                </div>
+
+                <div className="flex pl-4 text-[14px] border-r-transparent items-center text-[rgba(0,0,0,0.45)]  hover:text-[#262626] py-2 cursor-pointer transition-all delay-500 hover:bg-[#bfbfbfe1] hover:border-r-[#5F5F5F] border-r-solid border-r-2 ">
+                  <div className="h-[14px] w-[14px] mr-2 rounded-[50%] bg-[#52C41A]"></div>{" "}
+                  Green
+                </div>
+
+                <div className="flex pl-4 text-[14px] border-r-transparent items-center text-[rgba(0,0,0,0.45)]  hover:text-[#262626] py-2 cursor-pointer transition-all delay-500 hover:bg-[#bfbfbfe1] hover:border-r-[#5F5F5F] border-r-solid border-r-2 ">
+                  <FaPlus size={20} className="mr-2 flex" /> Add More Tags
+                </div>
+              </div>
+
+              <div className="mb-2">
+                <span className="pl-4 text-[14px] font-[500] mb-3 block text-[#595959]">
+                  More
+                </span>
+
+                <div className="flex pl-4 text-[14px] border-r-transparent items-center text-[rgba(0,0,0,0.45)]  hover:text-[#262626] py-2 cursor-pointer transition-all delay-500 hover:bg-[#bfbfbfe1] hover:border-r-[#5F5F5F] border-r-solid border-r-2 ">
+                  <FaRegClock className="mr-2" size={16} /> Recents
+                </div>
+
+                <div className="flex pl-4 text-[14px] border-r-transparent items-center text-[rgba(0,0,0,0.45)]  hover:text-[#262626] py-2 cursor-pointer transition-all delay-500 hover:bg-[#bfbfbfe1] hover:border-r-[#5F5F5F] border-r-solid border-r-2 ">
+                  <FaTrash className="mr-2" size={16} /> Trash
+                </div>
+              </div>
             </div>
-        </Link></div>
 
-      <div className="flex flex-col h-full">
+            <div className="border-t-[#D9D9D9] min-h-[223px] border-t-[1px] pt-3">
+              <div className="flex pl-4 items-center mb-3">
+                <BsCloudy className="mr-3" size={20} />
+                <span className="font-[500] text-[14px] block text-[#595959]">
+                  Storage
+                </span>
+              </div>
 
-      <div className="cusscroller h-[calc(100% - 1.25rem - 0.75rem - 27px)] overflow-y-scroll overflow-x-hidden">
-        
+              <div className="px-4 mb-1">
+                <span className="text-[14px]">1.18 GB of 150 GB </span>
+              </div>
 
-        <div className="mb-2">
-        <span className="pl-4 font-[500] mb-3 block text-[#595959]">
-          Drive Storage
-        </span>
+              <div className="px-4">
+                <LinearProgress
+                  variant="determinate"
+                  sx={{
+                    height: 6,
+                    borderRadius: "5rem",
+                    backgroundColor: "#D9D9D9",
+                  }}
+                  value={40}
+                />
+              </div>
 
-        <div className="flex pl-4 text-[14px] border-r-transparent items-center text-[rgba(0,0,0,0.45)]  hover:text-[#262626] py-2 cursor-pointer transition-all delay-500 hover:bg-[#bfbfbfe1] hover:border-r-[#5F5F5F] border-r-solid border-r-2 ">
-            <FaRegFolderOpen className="mr-2" size={20} /> My Drive
-        </div>
-          
-
-        <div className="flex pl-4 text-[14px] border-r-transparent items-center text-[rgba(0,0,0,0.45)]  hover:text-[#262626] py-2 cursor-pointer transition-all delay-500 hover:bg-[#bfbfbfe1] hover:border-r-[#5F5F5F] border-r-solid border-r-2 ">
-            <TbUsers className="mr-2" size={20} /> Shared with me
-        </div>
-
-        </div>
-
-          <div className="mb-2">
-        <span className="pl-4 font-[500] text-[14px] mb-3 block text-[#595959]">
-          Tags
-        </span>
-
-        <div className="flex pl-4 text-[14px] border-r-transparent items-center text-[rgba(0,0,0,0.45)]  hover:text-[#262626] py-2 cursor-pointer transition-all delay-500 hover:bg-[#bfbfbfe1] hover:border-r-[#5F5F5F] border-r-solid border-r-2 ">
-            <div className="h-[14px] w-[14px] mr-2 rounded-[50%] bg-[#FF4D4F]"></div> Red
-        </div>
-          
-
-        <div className="flex pl-4 text-[14px] border-r-transparent items-center text-[rgba(0,0,0,0.45)]  hover:text-[#262626] py-2 cursor-pointer transition-all delay-500 hover:bg-[#bfbfbfe1] hover:border-r-[#5F5F5F] border-r-solid border-r-2 ">
-              <div className="h-[14px] w-[14px] mr-2 rounded-[50%] bg-[#FADB14]"></div> Yellow
-        </div>
-
-        <div className="flex pl-4 text-[14px] border-r-transparent items-center text-[rgba(0,0,0,0.45)]  hover:text-[#262626] py-2 cursor-pointer transition-all delay-500 hover:bg-[#bfbfbfe1] hover:border-r-[#5F5F5F] border-r-solid border-r-2 ">
-              <div className="h-[14px] w-[14px] mr-2 rounded-[50%] bg-[#40A9FF]"></div> Blue
-        </div>
-
-         <div className="flex pl-4 text-[14px] border-r-transparent items-center text-[rgba(0,0,0,0.45)]  hover:text-[#262626] py-2 cursor-pointer transition-all delay-500 hover:bg-[#bfbfbfe1] hover:border-r-[#5F5F5F] border-r-solid border-r-2 ">
-              <div className="h-[14px] w-[14px] mr-2 rounded-[50%] bg-[#52C41A]"></div> Green
-        </div>
-
-
-         <div className="flex pl-4 text-[14px] border-r-transparent items-center text-[rgba(0,0,0,0.45)]  hover:text-[#262626] py-2 cursor-pointer transition-all delay-500 hover:bg-[#bfbfbfe1] hover:border-r-[#5F5F5F] border-r-solid border-r-2 ">
-           <FaPlus size={20} className="mr-2 flex" /> Add More Tags
-        </div>
-
-        </div>
-
-       <div className="mb-2">
-        <span className="pl-4 text-[14px] font-[500] mb-3 block text-[#595959]">
-          More
-        </span>
-
-        <div className="flex pl-4 text-[14px] border-r-transparent items-center text-[rgba(0,0,0,0.45)]  hover:text-[#262626] py-2 cursor-pointer transition-all delay-500 hover:bg-[#bfbfbfe1] hover:border-r-[#5F5F5F] border-r-solid border-r-2 ">
-            <FaRegClock className="mr-2" size={16} /> Recents
-        </div>
-          
-
-        <div className="flex pl-4 text-[14px] border-r-transparent items-center text-[rgba(0,0,0,0.45)]  hover:text-[#262626] py-2 cursor-pointer transition-all delay-500 hover:bg-[#bfbfbfe1] hover:border-r-[#5F5F5F] border-r-solid border-r-2 ">
-            <FaTrash className="mr-2" size={16} /> Trash
-        </div>
-
-        </div>
-
-        </div>
-
-        <div className="border-t-[#D9D9D9] min-h-[223px] border-t-[1px] pt-3">
-
-          <div className="flex pl-4 items-center mb-3">
-          <BsCloudy className="mr-3" size={20}/>
-          <span className="font-[500] text-[14px] block text-[#595959]">
-          Storage
-        </span>
-          </div>
-
-          <div className="px-4 mb-1">
-            <span className="text-[14px]">1.18 GB of 150 GB </span>
-          </div>
-
-          <div className="px-4">
-              <LinearProgress variant="determinate" sx={{
-                height: 6,
-                borderRadius: '5rem',
-                backgroundColor: '#D9D9D9'
-              }} value={40}/>
-          </div>
-
-          <div className="px-4 pb-10 mt-4 flex justify-center">
-              <Button className="text-[14px] text-[#000] w-full hover:text-white hover:bg-[rgba(0,0,0,0.3)]  capitalize" variant="contained">
-                Upgrade Storage Size
-              </Button>
-          </div>
-
-
-        </div>
-
-        </div>
-    </div>
-    <div className="w-full pl-[236px] h-full flex flex-col">
-        <div className="px-5 border-l border-r h-full">
-          <div className="flex justify-between py-4">
-            <div className="font-semibold text-4xl text-black">My Cloud</div>
-            <div className="">
-              <button className="py-2 px-8 bg-[#1890FF] text-white rounded-md">
-                Upload
-              </button>
+              <div className="px-4 pb-10 mt-4 flex justify-center">
+                <Button
+                  className="text-[14px] text-[#000] w-full hover:text-white hover:bg-[rgba(0,0,0,0.3)]  capitalize"
+                  variant="contained"
+                >
+                  Upgrade Storage Size
+                </Button>
+              </div>
             </div>
           </div>
+        </div>
+        <div className="w-full pl-[236px] h-full flex flex-col">
+          <div className="px-5 border-l border-r h-full">
+            <div className="flex justify-between py-4">
+              <div className="font-semibold text-[28px] text-black">
+                My Drive
+              </div>
+              <div className="">
+                <button className="py-2 flex items-center px-8 bg-[#1890FF] text-white rounded-md text-[16px] transition-all delay-300 hover:bg-[#0c75d6] font-[300]">
+                  <FaPlus size={12} className="mr-2" /> Upload
+                </button>
+              </div>
+            </div>
+            <div className="px-1">
+              <div className="flex items-center justify-between pt-7">
+                <Paper
+                  component="form"
+                  sx={{
+                    display: "flex",
+                    alignItems: "center",
+                    width: "calc(100% - 35px)",
+                    fontSide: 16,
+                    height: 42,
+                    border: "1px solid #00000073",
+                    boxShadow: "none",
+                  }}
+                  onSubmit={() => false}
+                >
+                  <InputBase
+                    sx={{ ml: 1, flex: 1, color: "#999" }}
+                    placeholder="Search File, Folder, Drive name"
+                    inputProps={{ "aria-label": "search" }}
+                  />
+                  <IconButton
+                    type="submit"
+                    sx={{
+                      p: "12px",
+                      borderLeft: "1px solid #00000073",
+                      borderRadius: 0,
+                    }}
+                    aria-label="search"
+                  >
+                    <TbSearch size={16.07} color="#00000073" />
+                  </IconButton>
+                </Paper>
+                <div className="cursor-pointer">
+                  <BsGrid3X3Gap size={19} color="#00000073" />
+                </div>
+              </div>
 
-          <div className="flex justify-center pt-7">
-            <Paper
-              component="form"
-              sx={{
-                p: "2px 4px",
-                display: "flex",
-                alignItems: "center",
-                width: 600,
-              }}
-            >
-              <InputBase
-                sx={{ ml: 1, flex: 1 }}
-                placeholder="Search for File or Folder"
-                inputProps={{ "aria-label": "search" }}
-              />
-              <IconButton type="submit" sx={{ p: "10px" }} aria-label="search">
-                <FaSearch color="#1890FF" />
-              </IconButton>
-            </Paper>
-          </div>
+              <div
+                style={{
+                  gridTemplateColumns: "repeat(auto-fill, minmax(186px, 1fr))",
+                }}
+                className="pt-7 grid gap-2 grid-flow-dense"
+              >
+                <Folder
+                  data={{
+                    name: "slide",
+                    size: 224,
+                    files: 10,
+                  }}
+                />
 
-          <div className="pt-8">
-            <Folder />
+                <Folder
+                  data={{
+                    name: "slide main",
+                    size: 224,
+                    files: 10,
+                  }}
+                />
+              </div>
+            </div>
           </div>
         </div>
-    </div>
-    <div className="h-full min-w-[250px] w-[250px]">
-        too
-    </div>
-    </div>)
+        <div className="h-full min-w-[250px] w-[250px]">too</div>
+      </div>
+    </>
+  );
+    
 }
 
 
